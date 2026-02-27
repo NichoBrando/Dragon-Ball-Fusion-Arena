2b6a8b9c0fb0e6b5fc1f2303feb6fca3dc021ee1
Nico <nicholas.computacao@outlook.com>
2026-02-27 12:19:52 -0300

    feat: added all FB09 energy markers

--- scripts/addNewCards.js
+++ scripts/addNewCards.js
@@ -3,6 +3,8 @@ const { writeFileSync, readFileSync } = require('fs');
 const path = require('path');
 const transformManualCardData = require('./steps/transformManualCardData');
 const manualAddCards = require('./manualAddCards.json');
+const markers = require('./markers.json');
+const saveImage = require('./steps/saveImage');
 
 const generateCardList = async () => {
     const manualAddCardsFormatted = await (async () => {
@@ -28,14 +30,44 @@ const generateCardList = async () => {
 
     const cardListSerialized = JSON.parse(curCardListStr);
 
-    console.log(cardListSerialized);
-
-    console.log(Object.keys(manualAddCardsFormatted).length);
-
-    console.log(manualAddCardsFormatted);
-
     manualAddCardsFormatted.forEach(item => {
         cardListSerialized[item.id] = item;
+    });
+
+    const transformedMarkers = markers.map(item => {
+        const name = (item.code).split('_')[0] + " Energy Marker";
+        
+        return ({
+            "id": item.code,
+            "isToken": true,
+            "face": {
+                "front": {
+                    name,
+                    "type": "Energy Marker",
+                    "cost": null,
+                    "image": item.image,
+                    "isHorizontal": false
+                }
+            },
+            name,
+            "type": "Energy Marker",
+            "cost": null,
+            tokens: [item.code]
+        })
+    });
+
+    await saveImage(transformedMarkers);
+
+    transformedMarkers.forEach(item => {
+        cardListSerialized[item.id] = {
+            ...item,
+            face: {
+                front: {
+                    ...item,
+                    image: `https://nichobrando.github.io/Dragon-Ball-Fusion-Arena/${item.id.split('-')[0]}/${item.id}.webp`
+                }
+            }
+        };
     });
 
     await writeFileSync(path.join(__dirname, "..", "docs", "fusionWorldCardList.json"), JSON.stringify(cardListSerialized));
